96788bc6211fb76224586b429833e67e463d9b28
Alexander Boev <97745889+xfoxcom@users.noreply.github.com>
2022-08-24 16:59:25 +0300

    + search by color or reg number

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -1,73 +1,203 @@
-package parking
 
-import java.util.function.Predicate
+import java.util.*
+import kotlin.random.Random
 
 fun main() {
-    var park = mutableListOf<String>()
-    loop@ while (true) {
-        val command = readln().split(" ")
-        when (command[0]) {
-            "spot_by_reg" -> {
+    print("How many mines do you want on the field? ")
+    val scanner = Scanner(System.`in`)
+    val mines = scanner.nextInt()
+    val list = mutableListOf(
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", ".")
+    )
+    val hided = mutableListOf(
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", ".")
+    )
 
-            }
-            "spot_by_color" -> {
+    val newList = mutableListOf<Int>()
+    while (newList.distinct().size != mines) {
+        newList.add(Random.nextInt(0, 89))
+        newList.removeAll(mutableListOf(9, 19, 29, 39, 49, 59, 69, 79))
+    }
 
-            }
-            "reg_by_color" -> {
-                if (park.isEmpty()) {
-                    println("Sorry, a parking lot has not been created.")
-                } else {
-                    val color = command[1]
-                    val list = mutableListOf<String>()
-                    for (s in park) {
-                        if (s != "") {
-                            val car = s.split("\\s+".toRegex())[0]
-                            val carColor = s.split("\\s+".toRegex())[1]
-                            if (carColor.lowercase() == color.lowercase()) list.add(car)
-                        }
+    for (i in newList) {
+        if (i in 0..8) list[0][i] = "X" else list[i/10][i%10] = "X"
+    }
+
+    var value : Any
+    value = 5
+    value = 9
+    value = " "
+
+    scan(list)
+    showField(hided)
+
+    while (!winCondition(list, hided)) {
+        print("Set/unset mine marks or claim a cell as free: ")
+        val (a, b, c) = readln().split("\\s+".toRegex())
+        when(c) {
+            "free" -> if (list[b.toInt() - 1][a.toInt() - 1].matches("[0-9]+".toRegex())) {
+                hided[b.toInt() - 1][a.toInt() - 1] = list[b.toInt() - 1][a.toInt() - 1]
+                showField(hided)
+            } else if (list[b.toInt() - 1][a.toInt() - 1].matches("\\.".toRegex())) {
+                reveal(list, hided, b.toInt() - 1, a.toInt() - 1)
+                reveal(list, hided, b.toInt() - 1, a.toInt() - 1)
+                showField(hided)
+            } else if (list[b.toInt() - 1][a.toInt() - 1].matches("X".toRegex())) {
+                for (i in 0..8) {
+                    for (j in 0..8) {
+                        if (list[i][j].matches("X".toRegex())) hided[i][j] = list[i][j]
                     }
-                    println(list.joinToString(", "))
-                    if (list.isEmpty()) println("No cars with color $color were found.")
+                }
+                showField(hided)
+                print("You stepped on a mine and failed!")
+                return
+            }
+            "mine" ->  if (hided[b.toInt() - 1][a.toInt() - 1].matches("\\*".toRegex())) {
+                hided[b.toInt() - 1][a.toInt() - 1] = "."
+                showField(hided)
+                if (winCondition(list, hided)) {
+                    println("Congratulations! You found all the mines!")
                 }
             }
-            "create" -> {
-                park = createPark(command[1].toInt())
-                println("Created a parking lot with ${park.size} spots.")
-            }
-            "park" -> if (park.isEmpty()) println("Sorry, a parking lot has not been created.") else
-                if (park.stream().noneMatch(Predicate.isEqual(""))
-            ) println("Sorry, the parking lot is full.") else {
-                for (i in park.indices) {
-                    if (park[i].isEmpty()) {
-                        println("${command[2]} car parked in spot ${i + 1}.")
-                        park[i] = command[1] + " " + command[2]
-                        continue@loop
-                    }
+            else if
+                         (hided[b.toInt() - 1][a.toInt() - 1].matches("\\.".toRegex())) {
+                hided[b.toInt() - 1][a.toInt() - 1] = "*"
+                showField(hided)
+                if (winCondition(list, hided)) {
+                    println("Congratulations! You found all the mines!")
                 }
-        }
-            "status" -> if (park.isEmpty()) println("Sorry, a parking lot has not been created.") else status(park)
-            "leave" -> if (park.isEmpty()) println("Sorry, a parking lot has not been created.") else if (park[command[1].toInt() - 1].isNotEmpty()) {
-                println("Spot ${command[1]} is free.")
-                park[command[1].toInt() - 1] = ""
-            } else println("There is no car in spot ${command[1]}.")
-            "exit" -> return
+            }
         }
     }
 }
-fun createPark(spots : Int) : MutableList<String> {
-    val park = mutableListOf<String>()
-    for (i in 0 until spots) {
-        park.add("")
+fun reveal (openField: MutableList<MutableList<String>>, hiddenField: MutableList<MutableList<String>>, b:Int, a:Int) {
+
+    if (b < 0 || b > 8 || a < 0 || a > 8 ) return
+
+    if (openField[b][a].matches("\\d".toRegex())) {
+        hiddenField[b][a] = openField[b][a]
+        return
     }
-    return park
+    if (hiddenField[b][a] != "." || openField[b][a] == "X") return
+
+    hiddenField[b][a] = "/"
+    replaceStar(hiddenField, openField)
+
+    reveal(openField, hiddenField, b - 1, a)
+    reveal(openField, hiddenField, b, a + 1)
+    reveal(openField, hiddenField, b, a - 1)
+    reveal(openField, hiddenField, b + 1, a)
+
+    reveal(openField, hiddenField, b - 1, a - 1)
+    reveal(openField, hiddenField, b + 1, a + 1)
+    reveal(openField, hiddenField, b - 1, a + 1)
+    reveal(openField, hiddenField, b + 1, a - 1)
 }
-fun status(park : MutableList<String>) {
-    var count = 0
-    for (i in 0 until park.size) {
-        if (park[i].isNotEmpty()) {
-            count++
-            println("${i + 1} ${park[i]}")
+fun scan (line: MutableList<MutableList<String>>) {
+    for (i in 0..8) {
+        for (j in 0..8) {
+            var count = 0
+            if (line[i][j] != "X") {
+                try { if (line[i-1][j-1] == "X") count++  } catch (e: IndexOutOfBoundsException) { e.message }
+                try { if (line[i-1][j] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+                try { if (line[i-1][j+1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+                try { if (line[i][j+1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+                try { if (line[i][j-1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+                try { if (line[i+1][j-1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+                try { if (line[i+1][j] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+                try { if (line[i+1][j+1] == "X") count++ } catch (e: IndexOutOfBoundsException) { e.message }
+                if (count > 0) line[i][j] = "$count"
+            }
         }
     }
-    if (count == 0) println("Parking lot is empty.")
+}
+fun showField (list: MutableList<MutableList<String>>) {
+    var i = 1
+    println(" │123456789│")
+    println("—|—————————|")
+    for (strings in list) {
+        print("${i++}|")
+        for (string in strings) {
+            print(string)
+        }
+        print("|")
+        println()
+    }
+    println("—|—————————|")
+}
+fun winCondition (openField: MutableList<MutableList<String>>, hiddenField: MutableList<MutableList<String>>): Boolean {
+    val list = mutableListOf(
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", ".")
+    )
+    val listH = mutableListOf(
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", "."),
+        mutableListOf(".", ".", ".", ".", ".", ".", ".", ".", ".")
+    )
+
+    for (i in 0..8) {
+        for (j in 0..8) {
+            if (openField[i][j] == "X") list[i][j] = "*"
+        }
+    }
+    for (i in 0..8) {
+        for (j in 0..8) {
+            if (hiddenField[i][j] == "*") listH[i][j] = "*"
+        }
+    }
+    return list == listH
+}
+fun replaceStar (hiddenField: MutableList<MutableList<String>>, openField: MutableList<MutableList<String>>) {
+    for (i in 0..8) {
+        for (j in 0..8) {
+            try {
+                if (hiddenField[i][j] == "*" && checkIf(hiddenField, i, j) && !openField[i][j].matches("\\d".toRegex())
+                )
+                    hiddenField[i][j] = "/"
+            } catch (e: IndexOutOfBoundsException) {
+                e.message
+            }
+        }
+    }
+}
+fun checkIf (hiddenField: MutableList<MutableList<String>>, i:Int, j:Int): Boolean {
+    try { if (hiddenField[i-1][j-1] == "/") return true } catch (e: IndexOutOfBoundsException) { e.message }
+    try { if (hiddenField[i-1][j] == "/") return true } catch (e: IndexOutOfBoundsException) { e.message }
+    try { if (hiddenField[i-1][j+1] == "/") return true } catch (e: IndexOutOfBoundsException) { e.message }
+    try { if (hiddenField[i][j+1] == "/") return true } catch (e: IndexOutOfBoundsException) { e.message }
+    try { if (hiddenField[i][j-1] == "/") return true } catch (e: IndexOutOfBoundsException) { e.message }
+    try { if (hiddenField[i+1][j-1] == "/") return true } catch (e: IndexOutOfBoundsException) { e.message }
+    try { if (hiddenField[i+1][j] == "/") return true } catch (e: IndexOutOfBoundsException) { e.message }
+    try { if (hiddenField[i+1][j+1] == "/") return true } catch (e: IndexOutOfBoundsException) { e.message }
+    return false
 }
