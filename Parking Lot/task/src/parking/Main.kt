0d4728c7f7fd08314fe1091d02d944610982140e
xfoxcom <xfoxcom@gmail.com>
2022-08-24 16:11:36 +0300

    + find by color

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -3,14 +3,41 @@ package parking
 import java.util.function.Predicate
 
 fun main() {
-    val park = mutableListOf("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
-   loop@ while (true) {
+    var park = mutableListOf<String>()
+    loop@ while (true) {
         val command = readln().split(" ")
         when (command[0]) {
-            "park" -> if (park.stream()
-                    .noneMatch(Predicate.isEqual(""))
+            "spot_by_reg" -> {
+
+            }
+            "spot_by_color" -> {
+
+            }
+            "reg_by_color" -> {
+                if (park.isEmpty()) {
+                    println("Sorry, a parking lot has not been created.")
+                } else {
+                    val color = command[1]
+                    val list = mutableListOf<String>()
+                    for (s in park) {
+                        if (s != "") {
+                            val car = s.split("\\s+".toRegex())[0]
+                            val carColor = s.split("\\s+".toRegex())[1]
+                            if (carColor.lowercase() == color.lowercase()) list.add(car)
+                        }
+                    }
+                    println(list.joinToString(", "))
+                    if (list.isEmpty()) println("No cars with color $color were found.")
+                }
+            }
+            "create" -> {
+                park = createPark(command[1].toInt())
+                println("Created a parking lot with ${park.size} spots.")
+            }
+            "park" -> if (park.isEmpty()) println("Sorry, a parking lot has not been created.") else
+                if (park.stream().noneMatch(Predicate.isEqual(""))
             ) println("Sorry, the parking lot is full.") else {
-                for (i in 0..19) {
+                for (i in park.indices) {
                     if (park[i].isEmpty()) {
                         println("${command[2]} car parked in spot ${i + 1}.")
                         park[i] = command[1] + " " + command[2]
@@ -18,11 +45,29 @@ fun main() {
                     }
                 }
         }
-            "leave" -> if (park[command[1].toInt() - 1].isNotEmpty()) {
+            "status" -> if (park.isEmpty()) println("Sorry, a parking lot has not been created.") else status(park)
+            "leave" -> if (park.isEmpty()) println("Sorry, a parking lot has not been created.") else if (park[command[1].toInt() - 1].isNotEmpty()) {
                 println("Spot ${command[1]} is free.")
                 park[command[1].toInt() - 1] = ""
             } else println("There is no car in spot ${command[1]}.")
             "exit" -> return
         }
     }
+}
+fun createPark(spots : Int) : MutableList<String> {
+    val park = mutableListOf<String>()
+    for (i in 0 until spots) {
+        park.add("")
+    }
+    return park
+}
+fun status(park : MutableList<String>) {
+    var count = 0
+    for (i in 0 until park.size) {
+        if (park[i].isNotEmpty()) {
+            count++
+            println("${i + 1} ${park[i]}")
+        }
+    }
+    if (count == 0) println("Parking lot is empty.")
 }
